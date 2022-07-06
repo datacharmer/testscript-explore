98f35b75d1d35a0c647b99794e28ec2691582d28
Giuseppe Maxia <g.maxia@gmail.com>
2022-07-06 21:14:30 +0200

    Update attempt1 test

--- attempt1/attempt1_test.go
+++ attempt1/attempt1_test.go
@@ -1,11 +1,12 @@
 package attempt1
 
 import (
-	"github.com/rogpeppe/go-internal/testscript"
 	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
 )
 
-func TestIntro(t *testing.T) {
+func TestAttempt1(t *testing.T) {
 	testscript.Run(t, testscript.Params{
 		Dir: "testdata",
 	})
